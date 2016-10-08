3d3c0ad7839c8d1e9758099dbd81fc1f33b8c9bb
jaqw <aswinjagannathan@gmail.com>
2016-10-08 15:33:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get
+app.get('/counter',function(req,res){
+    <counter=counter+1;
+    res.sendFile(path.join(__dirname,'ui','index.html'));
+});
 
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
